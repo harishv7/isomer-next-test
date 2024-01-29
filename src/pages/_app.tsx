e59fd32c414cc00a690d5f45555b03ab6e985a0b
Harish V <harish@open.gov.sg>
2024-01-29 17:36:02 +0800

    base setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
+import type { AppProps } from "next/app";
 import "@govtechsg/sgds/css/sgds.css";
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
